a9bf345519b614dfd585e109b9ec48aa5a4d7dc7
saladchalice <edwardlunited@gmail.com>
2025-02-19 02:05:36 -0800

    fixed link 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { fetchJSON, renderProjects } from './app.js';
 
 // only include first 3 projects
-const projects = await fetchJSON('/lib/projects.json');
+const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
