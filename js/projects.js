2b7c3657d5e6c488d82274a24178d4ddabce71e1
saladchalice <edwardlunited@gmail.com>
2025-02-19 01:52:54 -0800

    fixed link

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,6 @@
 import { fetchJSON, renderProjects } from './app.js';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('./lib/projects.json');
 
 // define the project container, class=projects
 const projectsContainer = document.querySelector('.projects');
