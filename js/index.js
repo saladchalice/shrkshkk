3b08a2ff5172db4d4cadea31ebd4253b10cfc9d2
saladchalice <edwardlunited@gmail.com>
2025-03-23 13:04:31 -0700

    gsap compass

--- js/index.js
+++ js/index.js
@@ -19,3 +19,59 @@ document.querySelectorAll("div").forEach(div => {
       console.log(`Scrolling detected in: ${this.id || "Unnamed div"}`);
   });
 });
+
+
+// compass scroll rotate animation 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(
+    ".option-north",
+    { opacity: 1, scale: 1, y: 0, x: "0%" }, 
+    { opacity: 1, scale: 1.3, y: -150, x: "-50%", rotation: 50,
+      scrollTrigger: {
+          trigger: ".compass-container",
+          start: "top 100px",
+          end: "bottom top",
+          scrub: true
+      }
+    }
+);
+
+gsap.fromTo(
+    ".option-south",
+    { opacity: 1, scale: 1, y: 0, x: "0%" }, 
+    { opacity: 1, scale: 1.3, y: 150, x: "-50%", rotation: -50,
+      scrollTrigger: {
+          trigger: ".compass-container",
+          start: "top 100px",
+          end: "bottom top",
+          scrub: true
+      }
+    }
+);
+
+gsap.fromTo(
+    ".option-east",
+    { opacity: 1, scale: 1, y: "0", x: 0 }, 
+    { opacity: 1, scale: 1.3, y: "-50%", x: 150, rotation: 180,
+      scrollTrigger: {
+          trigger: ".compass-container",
+          start: "top 100px",
+          end: "bottom top",
+          scrub: true
+      }
+    }
+);
+
+gsap.fromTo(
+    ".option-west",
+    { opacity: 1, scale: 1, y: "0%", x: 0 }, 
+    { opacity: 1, scale: 1.3, y: "-50%", x: -150, rotation: -180,
+      scrollTrigger: {
+          trigger: ".compass-container",
+          start: "top 100px",
+          end: "bottom top",
+          scrub: true
+      }
+    }
+);
