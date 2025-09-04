9fda9aa7db4740ec738d12d3210fe3f226816001
Edward Lu <edwardlunited@gmail.com>
2025-09-03 21:17:56 -0700

    Fixed Home Page Text Sizing

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
       <a href="index.html">Home</a>
       <a href="art-portfolio.html">Art</a>
       <a href="data-portfolio.html">Projects</a>
-      <a href="images/2025 Resume.pdf">CV</a>
+      <a href="images/2025 Resume.pdf" target="_blank" rel="noopener noreferrer">CV</a>
       <div class="dot"></div>
   `;
 
