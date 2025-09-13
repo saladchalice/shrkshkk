1902e83a67959d7a48dc8bfe258fd14e6f0fdb07
Edward Lu <edwardlunited@gmail.com>
2025-09-12 17:54:43 -0700

    Fit to Custom Domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/SharkShack/",
+  // For custom domain root deployment, use '/'
+  base: '/',
   plugins: [react()],
 })
