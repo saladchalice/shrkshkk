fe81af1b3941d37893ad28b753ee163e3bf39ff8
Edward Lu <edwardlunited@gmail.com>
2025-09-12 18:13:09 -0700

    Back to form

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  // For custom domain root deployment, use '/'
   base: '/',
   plugins: [react()],
 })
